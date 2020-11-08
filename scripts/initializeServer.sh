bb5c44892a919a8c74f760f6ac0101dfae32eead
Matthew Johnson <dev@matthewjohnson42.com>
2020-11-07 20:21:47 -0600

    Add loading spinner, prefetching of resources

--- scripts/initializeServer.sh
+++ scripts/initializeServer.sh
@@ -37,12 +37,11 @@ echo
 # copy files from source repo to nginx html directory
 sudo rm -rf /usr/share/nginx/html
 sudo mkdir /usr/share/nginx/html
-sudo cp ${PERSONAL_WEBSITE_HOME}/about.html ${NGINX_HTTP_HOME_DIR}/about.html
-sudo cp ${PERSONAL_WEBSITE_HOME}/index.html ${NGINX_HTTP_HOME_DIR}/index.html
-sudo cp ${PERSONAL_WEBSITE_HOME}/favicon.ico ${NGINX_HTTP_HOME_DIR}/favicon.ico
-sudo cp ${PERSONAL_WEBSITE_HOME}/style.css ${NGINX_HTTP_HOME_DIR}/style.css
-sudo cp -r ${PERSONAL_WEBSITE_HOME}/images/ ${NGINX_HTTP_HOME_DIR}/images/
-sudo cp -r ${PERSONAL_WEBSITE_HOME}/docs/ ${NGINX_HTTP_HOME_DIR}/docs/
+sudo cp ${PERSONAL_WEBSITE_HOME}/webpage/about.html ${NGINX_HTTP_HOME_DIR}/about.html
+sudo cp ${PERSONAL_WEBSITE_HOME}/webpage/index.html ${NGINX_HTTP_HOME_DIR}/index.html
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/css/ ${NGINX_HTTP_HOME_DIR}/css/
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/images/ ${NGINX_HTTP_HOME_DIR}/images/
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/docs/ ${NGINX_HTTP_HOME_DIR}/docs/
 # copy and link nginx configuration that specifies routing of http to the nginx html directory
 sudo cp ~/personal-website/nginx/matthewjohnson42.com /etc/nginx/sites-available/
 sudo rm -f /etc/nginx/sites-enabled/default
--- scripts/updateSiteFromSource.sh
+++ scripts/updateSiteFromSource.sh
@@ -26,12 +26,11 @@ git checkout master
 # copy files to nginx server directory
 sudo rm -rf /usr/share/nginx/html
 sudo mkdir /usr/share/nginx/html
-sudo cp ${PERSONAL_WEBSITE_HOME}/about.html ${NGINX_HTTP_HOME_DIR}/about.html
-sudo cp ${PERSONAL_WEBSITE_HOME}/index.html ${NGINX_HTTP_HOME_DIR}/index.html
-sudo cp ${PERSONAL_WEBSITE_HOME}/favicon.ico ${NGINX_HTTP_HOME_DIR}/favicon.ico
-sudo cp ${PERSONAL_WEBSITE_HOME}/style.css ${NGINX_HTTP_HOME_DIR}/style.css
-sudo cp -r ${PERSONAL_WEBSITE_HOME}/images/ ${NGINX_HTTP_HOME_DIR}/images/
-sudo cp -r ${PERSONAL_WEBSITE_HOME}/docs/ ${NGINX_HTTP_HOME_DIR}/docs/
+sudo cp ${PERSONAL_WEBSITE_HOME}/webpage/about.html ${NGINX_HTTP_HOME_DIR}/about.html
+sudo cp ${PERSONAL_WEBSITE_HOME}/webpage/index.html ${NGINX_HTTP_HOME_DIR}/index.html
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/css/ ${NGINX_HTTP_HOME_DIR}/css/
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/images/ ${NGINX_HTTP_HOME_DIR}/images/
+sudo cp -r ${PERSONAL_WEBSITE_HOME}/webpage/docs/ ${NGINX_HTTP_HOME_DIR}/docs/
 
 # copy scripts to home directory for system maintenance
 cp ${PERSONAL_WEBSITE_HOME}/scripts/updateSiteFromSource.sh ~
